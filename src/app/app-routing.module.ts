d255c1b78dba0699ffe2996b7e6dc87b49d623ad
ninajacques <nina.jacques@nextt.com.br>
2020-03-01 20:05:15 -0300

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from "./pages/main/main.component";
+import { MasterComponent } from './pages/master/master.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MapComponent } from './pages/map/map.component';
+import { CreateSaleComponent } from './pages/create-sale/create-sale.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: LoginComponent },
+  {
+    path: "",
+    component: MasterComponent,
+    children: [
+      {path: "main", component: MainComponent },
+      {path: "map", component: MapComponent },
+      {path: "create-sale", component: CreateSaleComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MasterComponent } from './pages/master/master.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MapComponent } from './pages/map/map.component';
+import { CreateSaleComponent } from './pages/create-sale/create-sale.component';
+import { SaleListComponent } from './components/sale-list/sale-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LoginComponent,
+    MasterComponent,
+    HeaderComponent,
+    FooterComponent,
+    MapComponent,
+    CreateSaleComponent,
+    SaleListComponent
   ],
   imports: [
     BrowserModule,
